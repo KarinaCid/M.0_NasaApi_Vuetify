8bb4310e80bd8246b89ea07e4c6608271d1a1b7e
Karina Cid <memento_mori.06@hotmail.com>
2020-11-05 20:05:42 -0300

    Initialize project, changes in:  Views: Apod, Login; index of Router and Store, App, Firebase & Main components

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,18 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const NASA_KEY = "bN3a1BYhIxkja97wnwLhubXIdcyy8RIka4ZNdfQI";
+
 export default new Vuex.Store({
   state: {
+    apod: {},
   },
   mutations: {
   },
   actions: {
+    getApod() {
+
+    }
   },
   modules: {
   }
